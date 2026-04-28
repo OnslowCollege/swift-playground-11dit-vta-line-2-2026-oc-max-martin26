54cf05580607c05b1be34450566e2efa8f863251
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-04-28 21:22:27 +0000

    broken but fixing lol

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,19 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-    }
+print("enter an adjective:")
+    let adjective1 = readLine()!
+  
+  print("enter a noun:")
+    let noun1 = readLine()!
+  
+  print("enter an adjective:")
+    let adjective2 = readLine()!
+   
+   print("enter an adjective:")
+    let adjective3 = readLine()!
+
+    let madlibstring = "pookie riley was doing his favourite thing in the whole wide world \(noun1) hew was having a great time until he was interupted by \(adjective1) and everyone \(adjective2) the \(adjective3)"
+    print (madlibstring)
 }
+    }
