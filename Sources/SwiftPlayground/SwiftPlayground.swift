6663428ca4a3c8bcb732bd59384e970f6bb9dc58
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-04-30 02:21:50 +0000

    Apr 30, 2026, 2:21 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,26 @@
 @main
 struct SwiftPlayground {
     static func main() {
+          let x = 15
+          let y = 2
 
+          let added = x + y
+          let subtracted = x - y
+          let multiplied = x * y 
+          let divided = x / y
+          let remainder = x % y
+
+          let added2 = y + x
+          let subtracted2 = y - x
+          let multiplied2 = y * x
+          let divided2 = y / x
+          let remainder2 = y % xw
+
+          print("\(x) + \(y) = \(added)")w
+          print("\(x) - \(y) = \(subtracted)"w)
+          print("\(x) * \(y) = \(multiplied)") 
+          print("\(x) / \(y) = \(divided)") w
+          print("\(x) % \(y) = \(remainder)") 
     }
     }
+    
