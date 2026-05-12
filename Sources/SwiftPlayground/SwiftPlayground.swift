c935f895f3153db498b11dfe7ecf8f8b00d3296b
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-05-12 21:45:42 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,26 +4,46 @@
 @main
 struct SwiftPlayground {
     static func main() {
-          let x = 15
-          let y = 2
+var counter = 0
+while counter < 10 {
+  print("Beep \(counter)") 
+  counter = counter + 1
+}
 
-          let added = x + y
-          let subtracted = x - y
-          let multiplied = x * y 
-          let divided = x / y
-          let remainder = x % y
+var isRunning = true
+var total = 0
+print ("welcome to Buc-ees")
 
-          let added2 = y + x
-          let subtracted2 = y - x
-          let multiplied2 = y * x
-          let divided2 = y / x
-          let remainder2 = y % x
 
-          print("\(x) + \(y) = \(added)")
-          print("\(x) - \(y) = \(subtracted)")
-          print("\(x) * \(y) = \(multiplied)") 
-          print("\(x) / \(y) = \(divided)") 
-          print("\(x) % \(y) = \(remainder)") 
+while isRunning {
+  print ("A. for burger: $6")
+  print ("B. for sandwich: $7")
+  print ("C. for cola: $3")
+  print ("Q to quit")
+  print ("Enter an option:")
+  let userInput = readLine()!
+
+  if userInput == "A" {  total = total + 6
+    print ("Did He Just Say That His Last Name... Is Burger?")
+    print (" Do you come with Fries? Is your middle name Cheese?")
+ 
+   } else if userInput == "B" { total = total + 7
+    print("thank you for your purchase")
+  
+   }else if userInput == "C" { total = total + 3
+     print("thank you for your purchase")
+  
+   }else if userInput == "Q" { print ("would you like to complete your order")
+   let userInput = readLine()!
+   if userInput == "yes" {print("thank you for shopping at buc-ees your total is \(total)")
+    } else if userInput == "no"{
+      print("keep buying chud")
     }
-    }
-    
+  
+       isRunning = false
+   }
+}
+
+
+}
+}
