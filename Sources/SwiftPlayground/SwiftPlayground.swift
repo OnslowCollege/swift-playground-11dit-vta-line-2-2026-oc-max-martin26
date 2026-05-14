28ee337d2d0ea34971c3e2772a7a33e258234a3d
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-05-14 02:51:57 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,44 @@
 @main
 struct SwiftPlayground {
     static func main() {
+var invitees:[String] = []
+var adding = true
+
+
+while adding{
+print("who do you want to invite")
+let invitee = readLine()!
+if invitee == ""{
+adding = false
+}else{
+invitees.append(invitee)
+}
+}
+var deleting = true
+while deleting{
+  print("this is the list you have for now")
+print (invitees)
+print ("who would you like to remove from the invitee list")
+let removedInvitee = readLine()!
+if removedInvitee == ""{
+ deleting = false
+}else{
+   if let index = invitees.firstIndex(of: "removedInvitee") {
+   invitees.remove(at: index)
+}
+}
+
+}
+
+
+
+print("the invitees are")
+invitees for
+
+
+
+
+
 
 
 
