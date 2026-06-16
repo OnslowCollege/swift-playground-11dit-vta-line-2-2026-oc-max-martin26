d107f3f4df36581f9a4be39696de08ef7dc443b8
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-06-16 21:18:57 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,7 +4,7 @@
 @main
 struct SwiftPlayground {
     var isRunning = true
-    
+
     
     let sleepAmount: [Int] = [0, 0, 0, 0, 0, 0, 0,]
     let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",]
