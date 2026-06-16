8c765b4f36500fa04c5fec4ed048834c2e40af55
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-06-16 21:57:05 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,13 +3,25 @@
 
 @main
 struct SwiftPlayground {
-    var isRunning = true
-
-    
-    let sleepAmount: [Int] = [0, 0, 0, 0, 0, 0, 0,]
-    let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",]
-
-    print
-    
     static func main() {
+    print("Welcome to Sleep Tracker.")
+    print("Let’s start by recording your sleep.")
+    print("Use 24 hour time.")
+    
+    var isRunning: Bool = true 
+    var sleepAmount: [Int] = [0, 0, 0, 0, 0, 0, 0,]
+    let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",]
+let time: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,]
+let x = (0)
+    print("What time did you go to sleep on \(days[x])?")
+    print("Enter a number:")
+    let sleepTime = readLine() 
+    
+    print("What time did you wake up on \(days[x+1])?")
+    print("Enter a number:")
+    let wakeTime = readLine()
+
+    let netSleep = 
+
+
     }}
