debef34ca38497164c4ba91955f02dcadd2a4414
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-06-18 03:08:10 +0000

    commit

--- Package.swift
+++ Package.swift
@@ -1,15 +1,28 @@
 // swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
 
-let package = Package(
-    name: "SwiftPlayground",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "SwiftPlayground"
-        ),
-    ]
-)
+struct SwiftPlayground {
+    static func main() {
+    print("Welcome to Sleep Tracker.")
+    print("Let’s start by recording your sleep.")
+    
+    
+    //my var
+    var sleepAmount: [Int] = [0, 0, 0, 0, 0, 0, 0,]
+    let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",]
+    var totalHours = 0
+
+let index = (0)
+    (0..<days.count).forEach{ index in
+    }
+    print("how long did you sleep on\(days[index])?")
+    print("Enter a number:")
+    let sleepTime = readLine()
+    sleepAmount[index] = sleepTime
+    sleepAmount.forEach{ sleepHour in
+    totalHours += sleepHour}
+    
+
+
+    }}
