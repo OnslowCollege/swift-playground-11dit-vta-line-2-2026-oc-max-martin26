6d1a161aa663cd73ccbae8c84b799898f25781e5
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-06-25 02:17:12 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -20,37 +20,56 @@ struct SwiftPlayground {
     var totalHours = 0
     //this is the limit for how much you can scroll every day
     var dayLimit = 0
-
+//had to add this to remove magic numbers cause fahh
+let addAmount = 1
     //this prints something
         print("Welcome to Screen Time Tracker.")
-    while isRunning == true {
-    print("What is the hour limit set for 1 day? Enter a number:")
     
-
+    while isRunning == true {
+    
+    //asks what your screentime limit for one day is and makes sure that it is an Integer and is within 24 hours
+    //then sets that to be the amount you are allowed to be on per day
+    print("What is the hour limit set for 1 day? Enter a number:")
     if let userInput = readLine(), var setLimit = Int(userInput), setLimit >= 0 && setLimit <= 24 {
     dayLimit += setLimit
     print("your screentime limit for 1 day is" ,[dayLimit])
     timeSpentOnSocialMedia() }
+    
+    //if type an invalid limit
     else { print("dayLimit invalid"); isRunning = false }
 
+
+    //this function says what day it is and runs the next function
+    func nextDay() {
     print("On",days[index], "how many hours did you spend on:")
+    timeSpentOnSocialMedia()}
+    
+    //this function prints a platform from the list and asks you how many hours you spent on it
     func timeSpentOnSocialMedia() {
     print (platforms[index2]) 
+    //this checks to see if you put in a valid amount of hours and if you did adds your hours to how long you have been on that day
     if let userInput = readLine(), var platformTime = Int(userInput), platformTime >= 0 && platformTime <= 24 {
     totalDaysHours += platformTime
     }
+    //if you put in invalid hours
     else { print("platformTime invalid"); isRunning = false }}
-    if index2 <= 5 {
-index2 += 1
+    
+    //increases index for the platform string and reruns the function asking you how long you spend on that platform
+    if index2 <= platforms.count {
+index2 += addAmount
     timeSpentOnSocialMedia()}
+    
+    //once you have cycled through all the platforms this adds how many hours you were on that day to your total hours and then goes to the next day
+    //this repeats until you have done the whole week
     else { index2 = 0
         print("Overall on", [index], "you spent", [totalDaysHours], "hours doom scrolling.")
-        index += 1
+        index += addAmount
         totalHours += totalDaysHours
         totalDaysHours = 0
-
-        timeSpentOnSocialMedia()}
-    if index >= 7 { finalTime()
+        nextDay()}
+    
+    //when you have finished the whole week this gives you your summary and lets you know if you were over or under the limit
+    if index >= days.count { finalTime()
         
     }
     
@@ -58,8 +77,8 @@ index2 += 1
     print("SUMMARY")
     print("=—————=") 
     print("- This week you spent", [totalHours],"hours doom scrolling")
-    print("- Average of", [totalHours/7], "hours per day")
-    let totalLimit = dayLimit * 7
+    print("- Average of", [totalHours/days.count], "hours per day")
+    let totalLimit = dayLimit * days.count
     if totalHours > totalLimit {
         print("Overall, your usage is OVER the limit!")
     }
