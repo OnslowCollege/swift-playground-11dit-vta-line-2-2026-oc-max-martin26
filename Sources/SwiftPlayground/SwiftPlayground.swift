ca4b14832a06aaeb24f56adf0a46a089ae0a1bcf
oc-max-martin26 <max.martin@student.onslow.school.nz>
2026-06-25 03:08:21 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,9 +8,7 @@ struct SwiftPlayground {
         var isRunning: Bool = true
 
         //my strings
-        let days: [String] = [
-            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
-        ]
+        let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         let platforms: [String] = ["Facebook", "SnapChat", "Instagram", "Discord", "Other"]
         //this index will be used for the days string
         var index = 0
@@ -32,12 +30,11 @@ struct SwiftPlayground {
             //asks what your screentime limit for one day is and makes sure that it is an Integer and is within 24 hours
             //then sets that to be the amount you are allowed to be on per day
             print("What is the hour limit set for 1 day? Enter a number:")
-            if let userInput = readLine(), var setLimit = Int(userInput),
-                setLimit >= 0 && setLimit <= 24
+            if let userInput = readLine(), var setLimit = Int(userInput),  setLimit >= 0 && setLimit <= 24
             {
                 dayLimit += setLimit
                 print("your screentime limit for 1 day is", [dayLimit])
-                nextDay()
+    
             }
 
             //if type an invalid limit
@@ -46,14 +43,16 @@ struct SwiftPlayground {
                 isRunning = false
             }
 
-            //this function says what day it is and runs the next function
-            func nextDay() {
+            //for loop to loop all the days
+            days.forEach {day in 
                 print("On", days[index], "how many hours did you spend on:")
-                timeSpentOnSocialMedia()
-            }
+                timeSpentOnSocialMedia()}
+            
 
             //this function prints a platform from the list and asks you how many hours you spent on it
+            
             func timeSpentOnSocialMedia() {
+                platforms.forEach {platform in
                 print(platforms[index2])
                 //this checks to see if you put in a valid amount of hours and if you did adds your hours to how long you have been on that day
                 if let userInput = readLine(), var platformTime = Int(userInput),
@@ -66,24 +65,24 @@ struct SwiftPlayground {
                     print("platformTime invalid")
                     isRunning = false
                 }
-            }
+            
 
             //increases index for the platform string and reruns the function asking you how long you spend on that platform
             if index2 <= platforms.count {
                 index2 += addAmount
-                timeSpentOnSocialMedia()
             }
+                
 
             //once you have cycled through all the platforms this adds how many hours you were on that day to your total hours and then goes to the next day
             //this repeats until you have done the whole week
-            else {
+            else if index2 > platforms.count{
                 index2 = 0
                 print("Overall on", [index], "you spent", [totalDaysHours], "hours doom scrolling.")
                 index += addAmount
                 totalHours += totalDaysHours
                 totalDaysHours = 0
-                nextDay()
             }
+            }}
 
             //when you have finished the whole week this gives you your summary and lets you know if you were over or under the limit
             if index >= days.count {
